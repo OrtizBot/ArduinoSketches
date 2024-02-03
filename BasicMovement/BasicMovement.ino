ced8e945eca7f8ed0b78f554ed5dd7d4cca71967
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2024-02-03 16:38:06 -0700

    Added in the library.

---
+++ BasicMovement/BasicMovement.ino
@@ -0,0 +1,12 @@
+#include "OrtizMovement.h"
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  moveForward(3000);
+
+  moveBackward(3000);
+
+  turnLeft(2000);
+
+  turnRight(2000);
+}
---
+++ BasicMovement/OrtizMovement.h
@@ -0,0 +1,11 @@
+#ifndef OrtizMovement_H
+#define OrtizMovement_H
+
+#include <Arduino.h>
+void turnRight(int duration);
+void moveBackward(int duration);
+void moveForward(int duration);
+
+void turnLeft(int duration);
+
+#endif
