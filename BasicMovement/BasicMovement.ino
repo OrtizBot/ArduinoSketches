59cc8ab31ba6b6ac1c58c5b3b2fe05066677d6b4
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2024-02-04 13:32:48 -0700

    Update BasicMovement.ino

--- BasicMovement/BasicMovement.ino
+++ BasicMovement/BasicMovement.ino
@@ -1,12 +1,13 @@
 #include "OrtizMovement.h"
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  moveForward(3000);
+ moveForward(3000);
 
-  moveBackward(3000);
+turnLeft(2000);
 
-  turnLeft(2000);
+moveBackward(3000);
 
-  turnRight(2000);
+turnRight(2000);
+
+
 }
