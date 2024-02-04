529051927b9d93316502b48a5fe9d60bc5b9eb3b
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2024-02-04 16:31:21 -0700

    A

--- Wemos D1 Mini/BasicMovement/OrtizMovement.cpp
+++ Wemos D1 Mini/BasicMovement/OrtizMovement.cpp
@@ -1,9 +1,9 @@
-#include "OrtizMovement.h"
+// Wemos D1 Mini pin configuration
+int motor1pin1 = D1;  // GPIO pin D1
+int motor1pin2 = D2;  // GPIO pin D2
+int motor2pin1 = D3;  // GPIO pin D3
+int motor2pin2 = D4;  // GPIO pin D4
 
-int motor1pin1 = 2;
-int motor1pin2 = 3;
-int motor2pin1 = 4;
-int motor2pin2 = 5;
 void setup() {
   // put your setup code here, to run once:
   pinMode(motor1pin1, OUTPUT);
@@ -11,7 +11,6 @@ void setup() {
   pinMode(motor2pin1, OUTPUT);
   pinMode(motor2pin2, OUTPUT);
 }
-
 
 void moveForward(int duration) {
   digitalWrite(motor1pin1, HIGH);
