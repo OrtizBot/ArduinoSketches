2e40f17ec916fa1d7482a11fcef46677bac95852
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2024-02-04 16:30:24 -0700

    Moved some stuff. Added the Wemos D1 Mini

---
+++ Wemos D1 Mini/BasicMovement/BasicMovement.ino
@@ -0,0 +1,11 @@
+#include "OrtizMovement.h"
+
+void loop() {
+  moveForward(3000);
+
+  turnLeft(2000);
+
+  moveBackward(3000);
+
+  turnRight(2000);
+}
---
+++ Wemos D1 Mini/BasicMovement/OrtizMovement.cpp
@@ -0,0 +1,54 @@
+#include "OrtizMovement.h"
+
+int motor1pin1 = 2;
+int motor1pin2 = 3;
+int motor2pin1 = 4;
+int motor2pin2 = 5;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+}
+
+
+void moveForward(int duration) {
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+
+  delay(duration);
+}
+
+void moveBackward(int duration) {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+
+  delay(duration);
+}
+
+void turnLeft(int duration) {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+
+  delay(duration);
+}
+
+void turnRight(int duration) {
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+
+  delay(duration);
+}
